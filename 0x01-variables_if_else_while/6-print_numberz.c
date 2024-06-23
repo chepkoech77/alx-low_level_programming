775b3429c298e2fb14557686c66f7b91e977d6d5
chepkoech77 <gracejohnson.koech@gmail.com>
2024-06-23 15:47:49 -0700

    six

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		putchar(digit + '0');
+	}
+	putchar('\n');
+	return (0);
+}

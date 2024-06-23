c81bab39b812d1ba5af76324aaabbcf1965b0d24
chepkoech77 <gracejohnson.koech@gmail.com>
2024-06-23 14:57:04 -0700

    four

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Writing a program to print text
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,2 +1,1 @@
-generates the assembly code of a C code and save it in an output file.
-compiles a C file and creates an executable named cisfun.
+prints exactly "Programming is like building a multilingual puzzle

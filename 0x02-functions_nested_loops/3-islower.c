f5a270d45dcab1ffdced466c1f900ec415e7eb42
chepkoech77 <gracejohnson.koech@gmail.com>
2024-06-27 05:12:06 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _islower - chech if a character is lowercase
+ * @c: param
+ * Return: 1 (success)
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+		return (1);
+	else
+		return (0);
+	_putchar('\n');
+}

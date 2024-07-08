f10cd2595c62a3bbf7796ae7b0c0a06155fe080e
chepkoech77 <gracejohnson.koech@gmail.com>
2024-07-08 08:37:58 -0700

    gemset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - fills memory with constant
+ * @s: Pointer to memory
+ * @b: Constant byte to fill the memory with
+ * @n: Number of bytes to fill
+ *
+ * Return: Pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
